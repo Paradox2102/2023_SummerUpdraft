d0f9cbfd28b2762323e446329558058cc6e871d6
BrisMR <122310852+BrisMR@users.noreply.github.com>
2023-05-02 16:16:18 -0700

    Created DriveSubsystem and ArcadeDrive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,11 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -21,6 +24,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
+  private final Joystick m_stick = new Joystick(0);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -45,6 +50,7 @@ public class RobotContainer {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
+      m_driveSubsystem.setDefaultCommand(new RunCommand(()->m_driveSubsystem.arcadeDrive(m_stick.getThrottle() > 0 ? -m_stick.getY(): m_stick.getY(), -m_stick.getX()), m_driveSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DriveSubsystem extends SubsystemBase {
+  private final WPI_TalonFX m_rightDrive = new WPI_TalonFX(Constants.Drive.k_rightDrive);
+  private final WPI_TalonFX m_rightFollower = new WPI_TalonFX(Constants.Drive.k_rightFollower);
+  private final WPI_TalonFX m_leftDrive = new WPI_TalonFX(Constants.Drive.k_leftDrive);
+  private final WPI_TalonFX m_leftFollower = new WPI_TalonFX(Constants.Drive.k_leftFollower);
+  private final DifferentialDrive m_drive = new DifferentialDrive(m_leftDrive, m_rightDrive);
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+    m_rightDrive.configFactoryDefault();
+    m_rightFollower.configFactoryDefault();
+    m_leftDrive.configFactoryDefault();
+    m_leftFollower.configFactoryDefault();
+    m_rightDrive.setInverted(TalonFXInvertType.CounterClockwise);
+    m_rightFollower.follow(m_rightDrive);
+    m_rightFollower.setInverted(TalonFXInvertType.FollowMaster);
+    m_leftDrive.setInverted(TalonFXInvertType.Clockwise);
+    m_leftFollower.follow(m_leftDrive);
+    m_leftFollower.setInverted(TalonFXInvertType.FollowMaster);
+  }
+
+  public void setPower (double rightPower, double leftPower) {
+    m_rightDrive.set(TalonFXControlMode.PercentOutput, rightPower);
+    m_leftDrive.set(TalonFXControlMode.PercentOutput, leftPower);
+  }
+
+  public void stop () {
+    setPower(0, 0);
+  }
+
+  public void arcadeDrive (double speed, double rotation) {
+    m_drive.arcadeDrive(speed, rotation);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
