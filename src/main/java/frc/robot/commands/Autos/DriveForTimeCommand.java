718ce1f34b6ad87914ab13b49d6e480fe1ef60a3
BrisMR <122310852+BrisMR@users.noreply.github.com>
2023-06-22 16:29:19 -0700

    started autos

--- src/main/java/frc/robot/commands/Autos/DriveForTimeCommand.java
+++ src/main/java/frc/robot/commands/Autos/DriveForTimeCommand.java
@@ -2,50 +2,46 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.Autos;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.ApriltagsCamera.Logger;
-import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class CalibrateDriveCommand extends CommandBase {
-  /** Creates a new TestSpeedCommand. */
-  //private static double k_power = 0.5;
-  //private static double k_speed = 5;
-  private static double k_maxSpeed = Constants.Drive.k_maxSpeed;
+public class DriveForTimeCommand extends CommandBase {
+  /** Creates a new DriveForTimeCommand. */
   DriveSubsystem m_subsystem;
-  public CalibrateDriveCommand(DriveSubsystem subsystem) {
+  private Timer m_timer;
+  private double m_time;
+  private static double k_speed = 4500;
+  public DriveForTimeCommand(DriveSubsystem subsystem, Double time) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_time = time;
     m_subsystem = subsystem;
     addRequirements(subsystem);
-    Logger.log("CalibrateDriveCommand", 0, "CalibrateDriveCommand");
+    m_timer.reset();
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    Logger.log("CalibrateDriveCommand", 0, "Initialize");
+    m_timer.start();
+    m_subsystem.setSpeed(k_speed, k_speed);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    m_subsystem.setSpeed((k_maxSpeed/2), (k_maxSpeed/2));
-    //m_subsystem.setPower(k_power, k_power);
-    //m_subsystem.setSpeedFPS(k_speed, k_speed);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_subsystem.stop();
-    Logger.log("CalibrateDriveCommand", 0, "End");
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_timer.get() > m_time;
   }
 }
