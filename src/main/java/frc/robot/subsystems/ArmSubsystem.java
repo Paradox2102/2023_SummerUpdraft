7de91fc66bacc6a948e35a8aa5783146e4367144
BrandonQ <31326650+bovlb@users.noreply.github.com>
2023-07-24 07:30:11 -0700

    Squashed commit of the following:
    commit 1be981792efc85fd076a6d3f90fa7bf6c1340e99
    Author: BrandonQ <31326650+bovlb@users.noreply.github.com>
    Date:   Mon Jul 24 07:24:19 2023 -0700
    
        Suggest a way to have all three joysticks control arcade drive without code change
    
    commit 73e496e1dfb328e8f9337bf697087de6ca003733
    Author: BrandonQ <31326650+bovlb@users.noreply.github.com>
    Date:   Mon Jul 24 07:22:23 2023 -0700
    
        Squashed commit of the following:
    
        commit 8ea3607928517c04ee4669d770fef4b55f88a666
        Author: Isa <108543156+bluskyz11@users.noreply.github.com>
        Date:   Thu Jul 20 16:29:53 2023 -0700
    
            Robot Container Cone Position Commands
    
            Main Changes:
            -Made my own joystick + port
            -Fixed the if statement of the Intake Subsystem
            -Adjusted the stall speed of the Intake Subsystem to remove the squeaky sound but still hold the cone
            -Removed a Stalled logger from the wrist subsystem
            -Manually tested different angles, extents, and wrist angles in robot container to figure what worked for each position
            -Determined that the lowest cone position would be hard and should be done last (have yet to do)
            -Created a new Hand Position command to separate the extent from the angles so the robot is less violent
            -Added a max power to the Arm Subsystem to make the robot less violent as well
    
        commit 6b9bf2a775d77387813febdf31e801b538a743b3
        Author: paulregnier2 <122309411+paulregnier2@users.noreply.github.com>
        Date:   Tue Jul 18 16:21:55 2023 -0700
    
            Fixed ExtentStall & made HandPositionCommand
    
        commit 97d25a309bf0dc382f4d54d485bf86cf04dad931
        Author: Isa <108543156+bluskyz11@users.noreply.github.com>
        Date:   Thu Jul 13 16:08:47 2023 -0700
    
            Merge changes
    
        commit b7e14cc1d2562de65d32eb8985764297b0de9c6e
        Merge: 9dc8fa5 e02ab18
        Author: Isa <108543156+bluskyz11@users.noreply.github.com>
        Date:   Thu Jul 13 16:08:36 2023 -0700
    
            Merge branch 'Isa' of https://github.com/Paradox2102/2023_SummerUpdraft into Isa
    
        commit 9dc8fa5c97ec56f8b9dbc46b3f9dd629878a4dc4
        Author: Isa <108543156+bluskyz11@users.noreply.github.com>
        Date:   Thu Jul 13 16:00:57 2023 -0700
    
            Comments + Reach F term
    
    commit 804f76324eb8734a30db2d6e8d313eb677ea94ba
    Author: BrandonQ <31326650+bovlb@users.noreply.github.com>
    Date:   Wed Jul 19 19:59:14 2023 -0700
    
        Squashed commit of the following:
    
        commit 6b9bf2a775d77387813febdf31e801b538a743b3
        Author: paulregnier2 <122309411+paulregnier2@users.noreply.github.com>
        Date:   Tue Jul 18 16:21:55 2023 -0700
    
            Fixed ExtentStall & made HandPositionCommand
    
        commit 97d25a309bf0dc382f4d54d485bf86cf04dad931
        Author: Isa <108543156+bluskyz11@users.noreply.github.com>
        Date:   Thu Jul 13 16:08:47 2023 -0700
    
            Merge changes
    
        commit b7e14cc1d2562de65d32eb8985764297b0de9c6e
        Merge: 9dc8fa5 e02ab18
        Author: Isa <108543156+bluskyz11@users.noreply.github.com>
        Date:   Thu Jul 13 16:08:36 2023 -0700
    
            Merge branch 'Isa' of https://github.com/Paradox2102/2023_SummerUpdraft into Isa
    
        commit 9dc8fa5c97ec56f8b9dbc46b3f9dd629878a4dc4
        Author: Isa <108543156+bluskyz11@users.noreply.github.com>
        Date:   Thu Jul 13 16:00:57 2023 -0700
    
            Comments + Reach F term

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -44,6 +44,7 @@ public class ArmSubsystem extends SubsystemBase {
   private static double k_d = 0.001;
   private static double k_f = 0.005;
   private static double k_l = 18;
+  private static double k_maxPower = 0.8;
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
     m_armMotor.restoreFactoryDefaults();
@@ -102,6 +103,9 @@ public class ArmSubsystem extends SubsystemBase {
     if (m_PIDOn) {
       power = getFTerm(m_setPoint) + m_armPID.calculate(getArmAngleDegrees(), m_setPoint);
     } 
+    if (Math.abs(power) > k_maxPower) {
+      power = k_maxPower * Math.signum(power);
+    }
     //set power to 0 if the arm has been stalled for over 0.2 seconds
     if (power > k_stallPower){
       if(Math.abs(m_armRelative.getVelocity()) < k_stallSpeed){
